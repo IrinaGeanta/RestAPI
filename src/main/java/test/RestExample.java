c3c22c1066b99b40a324003b8a2d06ce84463c04
Irina <irinadaniela.geanta@gmail.com>
2024-01-09 21:29:50 +0200

    commit initial

---
+++ src/main/java/test/RestExample.java
@@ -0,0 +1,11 @@
+package test;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class RestExample {
+    @Test
+    public void restExampleTest(){
+       
+    }
+}
