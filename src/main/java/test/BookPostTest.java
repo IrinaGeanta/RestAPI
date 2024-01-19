b96f3079acc169d1707ee523c1dae5ca58e28586
Irina <irinadaniela.geanta@gmail.com>
2024-01-19 10:05:47 +0200

    tema cu book

--- src/main/java/test/BookPostTest.java
+++ src/main/java/test/BookPostTest.java
@@ -1,28 +1,35 @@
 package test;
 
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
 
 public class BookPostTest {
 
     @Test
-    public void bookPostRestAssured(){
-        RestAssured.baseURI="https://fakerestapi.azurewebsites.net/api/v1/Books";
+    public void bookPostRestAssured() throws FileNotFoundException {
         File bookFile = new File("fakeBook.json");
-/*
+
         Response response = given().body(bookFile).
                 contentType(ContentType.JSON).
-                        post().
+                        post("https://fakerestapi.azurewebsites.net/api/v1/Books").
                 then().statusCode(200).
                 extract().response();
 
-        id = response.jsonPath().getString("id");*/
+        String id = response.jsonPath().getString("id");
+        assertEquals(id, "6");
+
     }
 }
